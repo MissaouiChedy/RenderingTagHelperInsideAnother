fe225c83445a9ab87daa8f9a48aade88c604dc8a
Chedy Missaoui <missaoui.chedy@gmail.com>
2017-02-20 16:43:51 +0100

    Created the WrapperTagHelper and the InnerTagHelper

---
+++ src/RenderingTagHelperInsideAnother/TagHelpers/InnerTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RenderingTagHelperInsideAnother.TagHelpers
+{
+    public class InnerTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "p";
+            output.Attributes.SetAttribute(new TagHelperAttribute("class", "inner-tag-helper"));
+            output.Content.SetHtmlContent("This is from the inner tag helper");
+        }
+    }
+}
---
+++ src/RenderingTagHelperInsideAnother/TagHelpers/WrapperTagHelper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace RenderingTagHelperInsideAnother.TagHelpers
+{
+    public class WrapperTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            
+            output.TagName = "div";
+            output.Attributes.SetAttribute(new TagHelperAttribute("class", "wrapper-tag-helper"));
+
+            string innerContent = renderInnerTagHelper();
+
+            string finalContent = $@"<p>Wrapper</p>
+{innerContent}";
+
+            output.Content.SetHtmlContent(finalContent);
+        }
+
+        /*
+         * This methods creates, processes and renders the InnerTagHelper
+         */
+        private string renderInnerTagHelper()
+        {
+            InnerTagHelper innerTagHelper = new InnerTagHelper();
+            //Create a TagHelperContext instance
+            TagHelperContext innerContext = new TagHelperContext(
+                new TagHelperAttributeList(),
+                new Dictionary<object, object>(),
+                uniqueId: Guid.NewGuid().ToString()
+            );
+            // Create a TagHelperOutput instance
+            TagHelperOutput innerOutput = new TagHelperOutput(
+                tagName: "",
+                attributes: new TagHelperAttributeList(),
+                getChildContentAsync: (useCachedResult, encoder) =>
+                     Task.Factory.StartNew<TagHelperContent>(
+                            () => new DefaultTagHelperContent()
+                     )
+            );
+            // Process the InnerTagHelper instance 
+            innerTagHelper.Process(innerContext, innerOutput);
+
+            // Render and return the tag helper attributes and content
+            return $"<{innerOutput.TagName} {renderHtmlAttributes(innerOutput)}>{innerOutput.Content.GetContent()}</{innerOutput.TagName}>";
+        }
+
+        /*
+         * Helper Method to render html attributes
+         */
+        private string renderHtmlAttributes(TagHelperOutput output)
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach (var attribute in output.Attributes)
+            {
+                builder.Append($"{attribute.Name}=\"{attribute.Value}\"");
+            }
+            return builder.ToString();
+        }
+        
+    }
+}
