9dbfdec6660d1121232760d9588e26505e056c7a
Chedy Missaoui <missaoui.chedy@gmail.com>
2017-03-26 16:57:30 +0100

    Migrated to MsBuild(VS2017) Updated the inner tag helper rendering

--- src/RenderingTagHelperInsideAnother/TagHelpers/WrapperTagHelper.cs
+++ src/RenderingTagHelperInsideAnother/TagHelpers/WrapperTagHelper.cs
@@ -3,18 +3,27 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Text.Encodings.Web;
+using System.IO;
 
 namespace RenderingTagHelperInsideAnother.TagHelpers
 {
     public class WrapperTagHelper : TagHelper
     {
+        private HtmlEncoder _encoder;
+
+        public WrapperTagHelper(HtmlEncoder encoder)
+        {
+            _encoder = encoder;
+        }
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             
             output.TagName = "div";
             output.Attributes.SetAttribute(new TagHelperAttribute("class", "wrapper-tag-helper"));
 
-            string innerContent = renderInnerTagHelper(context);
+            string innerContent = renderInnerTagHelper();
 
             string finalContent = $@"<p>Wrapper</p>
 {innerContent}";
@@ -25,38 +34,45 @@ namespace RenderingTagHelperInsideAnother.TagHelpers
         /*
          * This methods creates, processes and renders the InnerTagHelper
          */
-        private string renderInnerTagHelper(TagHelperContext context)
+        private string renderInnerTagHelper()
         {
             InnerTagHelper innerTagHelper = new InnerTagHelper();
-
+            var attributes = new TagHelperAttributeList();
+            
             // Create a TagHelperOutput instance
             TagHelperOutput innerOutput = new TagHelperOutput(
-                tagName: "",
-                attributes: new TagHelperAttributeList(),
+                tagName: "div",
+                attributes: attributes,
                 getChildContentAsync: (useCachedResult, encoder) =>
-                     Task.Factory.StartNew<TagHelperContent>(
-                            () => new DefaultTagHelperContent()
-                     )
+                     Task.Run<TagHelperContent>(() => new DefaultTagHelperContent())
+            )
+            {
+                TagMode = TagMode.StartTagAndEndTag
+            };
+            // Create a TagHelperContext instance
+            TagHelperContext innerContext = new TagHelperContext(
+                allAttributes: attributes,
+                items: new Dictionary<object, object>(), 
+                uniqueId: Guid.NewGuid().ToString()
             );
 
             // Process the InnerTagHelper instance 
-            innerTagHelper.Process(context, innerOutput);
-
+            innerTagHelper.Process(innerContext, innerOutput);
+            
             // Render and return the tag helper attributes and content
-            return $"<{innerOutput.TagName} {renderHtmlAttributes(innerOutput)}>{innerOutput.Content.GetContent()}</{innerOutput.TagName}>";
+            return renderTagHelperOutput(innerOutput);
         }
 
         /*
-         * Helper Method to render html attributes
+         * Helper Method to gather the html content
          */
-        private string renderHtmlAttributes(TagHelperOutput output)
+        private string renderTagHelperOutput(TagHelperOutput output)
         {
-            StringBuilder builder = new StringBuilder();
-            foreach (var attribute in output.Attributes)
+            using (var writer = new StringWriter())
             {
-                builder.Append($"{attribute.Name}=\"{attribute.Value}\"");
+                output.WriteTo(writer, _encoder);
+                return writer.ToString();
             }
-            return builder.ToString();
         }
         
     }
